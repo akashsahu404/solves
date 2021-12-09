eb697c8e4f128b846475874ea1774cf437336a00
Akash Sahu <95840934+akashsahu404@users.noreply.github.com>
2021-12-09 11:24:23 +0530

    Create division.py

---
+++ hacker-rank_python/division.py
@@ -0,0 +1,11 @@
+# The provided code stub reads two integers, a and b, from STDIN.
+
+# Add logic to print two lines.
+# The first line should contain the result of integer division,  a//b.
+# The second line should contain the result of float division,  a/b .
+
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+print(str(a//b))
+print(str(a/b)
